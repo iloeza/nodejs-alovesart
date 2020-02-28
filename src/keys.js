48b164d467b3c6c3177ddd6d03f46971f7a3865d
Ivan Loeza <ivan@coatilabs.com>
2020-02-27 20:37:03 -0600

    connection to remote db working fine

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        host: '162.241.60.249',
+        user: "dbuppcom_ivan",
+        password: "Kissmyass92",
+        database: "dbuppcom_asenetlovesart",
+        connectionLimit: 10
+    }
+}
